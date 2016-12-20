8e3a6afc5995a6fa7f832c35fbfcf0ea63391d47
BenjaminBini <benjamin.bini@gmail.com>
2016-12-20 17:02:26 +0100

    Suppression require inutile

--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 require 'compass/import-once/activate'
-require 'autoprefixer-rails'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
